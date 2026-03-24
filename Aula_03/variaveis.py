91aff63a582a927d83d486022953eedd0d34e19c
João Pedro Martins <joaopedromartinsr00@gmail.com>
2026-03-24 09:57:25 -0300

    alteração

---
+++ Aula_03/variaveis.py
@@ -0,0 +1,35 @@
+# Váriaveis e Tipo de Dados:
+# Em python, você não precisa declarar o tipo de uma variável antes de usá-la. O tipo é inferido automaticamente pelo interpretador.
+# EM Python, a variável é declarada no padrão snake_case.
+# 1. string ->
+# 2. number - int (Numero Inteiro) ->
+# 3. numer - float (Numero Decimal) ->
+# 4. booblen - True ou False (Verdadeiro ou Falso) ->
+nome = "João Pedro Martins" # string
+email = "joaopedromartinsr00@email.com" # string 
+cidade = "Rio de Janeiro" # string
+cpf = "12345678910" # number-int
+salario = "20000.99" # numer-float
+casado = "True" # boolen
+
+
+# Em pytho, a variável é delarada no padrão snake_case.
+# 1. booblen - True ou False (verdadeiro ou Falso) ->
+1 A Terra é plana. =
+2 desligue o computador agora = 
+3. X>5. =
+4. Existe vida em outros planetas.
+5. O céu é azul 
+6. Qual a capital da França?
+ 7. 2024 é um ano bissexto
+8 Esta sentença tem cinco palavras
+9 Obrigado pelo tempo
+10 Se a equipe entregar o módulo, então o projeto avança
+
+
+1. A Terra é plana. 6. Qual a capital da França?
+2. Desligue o computador agora! 7. 2024 é um ano bissexto
+3. X>5. 8. Esta sentença tem cinco
+4. Existe vida em outros planetas. palavras.
+5. O céu é azul 9. Obrigado pelo seu tempo.
+ 10. Se 
---
+++ aula_09/README.md
@@ -0,0 +1,31 @@
+# ANOTAÇÕES DE FUNDAMENTOS DA PROGRAMAÇÃO
+
+## Tipos de dados em python
+1. string
+2. number int
+3. number float
+4. boolean
+
+## Operadores matemáticos - básicos 
++ -> adição
+- -> subtração
+* -> multiplicação
+/ -> divisão
+
+## Operadores lógicos
+and -> e -> Se duas condições forem verdadeiras, o resultado é verdadeiro.
+or -> ou -> Ser pelo menos uma condição for verdadeira, o resultado é verdadeiro.
+not -> Ele altera o valor booleano da condição.
+
+
+## Métodos em Python
+1. print() -> exibe informações no terminal.
+2. inpuit() -> Capturar uma informção do terminal.
+
+# Format em Python 
+ 
+# Estrutura de repetição
+
+ "if (se)" -> Verifica se uma condição é true(verdadeira).Se for, ele executa o código.
+ "elif (senão se)" -> é usado para testar várias condições. Ele só executa se todas as condições anteriores forem falsas.
+ "else (senão)" -> Executa o código se a condição if for false(falso)
